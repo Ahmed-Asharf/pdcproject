3597337e34bad5f6063c5d391561210465611676
Ahmed-Asharf <56397528+Ahmed-Asharf@users.noreply.github.com>
2022-01-11 13:18:59 -0800

    Add files via upload

--- fileReadinginC.c
+++ fileReadinginC.c
@@ -4,8 +4,9 @@
 #include <mpi.h>
 
 
-int main()
+int main(int argc, char *argv[])
 {
+    // printf("%s",argv[2]);
     MPI_Init(NULL, NULL);
     int rank = 0;
     int world;
@@ -13,12 +14,11 @@ int main()
     char globalresults[100][1024];
     MPI_Comm_size(MPI_COMM_WORLD, &world);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // printf("%d\n",world);
+
     char results[100][1024];
     int index1=0;
     int threshold = (int)num/world;
-    if(rank == 0){ //master
-        //divide the input file
+    if(rank == 0){ 
         FILE* stream = fopen("file1.txt", "r");
         char line[2048];
         int count = 0;
@@ -47,65 +47,102 @@ int main()
         char local[threshold][9][50];
         MPI_Recv(&local, threshold*9*50, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         int x, y;
-        for(x = 0; x < threshold; x++){
-            if(strcmp(local[x][0],"Sir Mir Khan")==0)
-            {
-                char *str1 = local[x][0];
-                for(int i = 1 ; i < 9 ; i++)
-                {
-                    strcat(str1," ");
-                    strcat(str1, local[x][i]);
-                   
-                }
-                strcat(str1,"\0");
-                // printf("%s\n", str1);
-                strcpy(results[index1],str1); 
-                index1++; 
+        if(strcmp(argv[1],"name")==0){
+            for(x = 0; x < threshold; x++){
+                    if(strcmp(local[x][0],argv[2])==0)
+                    {
+                        char *str1 = local[x][0];
+                        for(int i = 1 ; i < 9 ; i++)
+                        {
+                            strcat(str1," ");
+                            strcat(str1, local[x][i]);
+                        
+                        }
+                        strcat(str1,"\0");
+                        strcpy(results[index1],str1); 
+                        index1++; 
+                    }
             }
-            else if(strcmp(local[x][1],"Muhammad Mureed")==0)
-            {
-                               char *str1 = local[x][0];
-                for(int i = 1 ; i < 9 ; i++)
-                {
-                    strcat(str1," ");
-                    strcat(str1, local[x][i]);
-                   
-                }
-                strcat(str1,"\0");
-                // printf("%s\n", str1);
-                strcpy(results[index1],str1);  
-                index1++; 
-            }
-            else if(strcmp(local[x][2],"68632-26254834-4")==0)
-            {
-                char *str1 = local[x][0];
-                for(int i = 1 ; i < 9 ; i++)
-                {
-                    strcat(str1," ");
-                    strcat(str1, local[x][i]);
-                   
-                }
-                strcat(str1,"\0");
-                strcpy(results[index1],str1);  
-                index1++; 
-            }
-            else if(strcmp(local[x][3],"48")==0)
-            {
-                char *str1 = local[x][0];
-                for(int i = 1 ; i < 9 ; i++)
-                {
-                    strcat(str1," ");
-                    strcat(str1, local[x][i]);
-                   
-                }
-                strcat(str1,"\0");
-              strcpy(results[index1],str1);  
-              index1++; 
-            }
-
         }
-
-
+        else if(strcmp(argv[1],"cnic")==0){
+            for(x = 0; x < threshold; x++){
+                if(strcmp(local[x][2],argv[2])==0)
+                {
+                    char *str1 = local[x][0];
+                    for(int i = 1 ; i < 9 ; i++)
+                    {
+                        strcat(str1," ");
+                        strcat(str1, local[x][i]);
+                    
+                    }
+                    strcat(str1,"\0");
+                    strcpy(results[index1],str1);  
+                    index1++; 
+                }
+            }
+        }
+        else if(strcmp(argv[1],"age")==0){
+            for(x = 0; x < threshold; x++){
+                if(strcmp(local[x][3],argv[2])==0)
+                {
+                    char *str1 = local[x][0];
+                    for(int i = 1 ; i < 9 ; i++)
+                    {
+                        strcat(str1," ");
+                        strcat(str1, local[x][i]);
+                    
+                    }
+                    strcat(str1,"\0");
+                    strcpy(results[index1],str1);  
+                    index1++; 
+                }
+            }
+        }
+        //  else if(argv[1] == "Cnic"){
+        //     for(x = 0; x < threshold; x++){
+        //         if(strcmp(local[x][1],"Muhammad Mureed")==0)
+        //         {
+        //                         char *str1 = local[x][0];
+        //             for(int i = 1 ; i < 9 ; i++)
+        //             {
+        //                 strcat(str1," ");
+        //                 strcat(str1, local[x][i]);
+                    
+        //             }
+        //             strcat(str1,"\0");
+        //             strcpy(results[index1],str1);  
+        //             index1++; 
+        //         }
+        //     }
+        // }
+            // else if(strcmp(local[x][2],"68632-26254834-4")==0)
+            // {
+            //     char *str1 = local[x][0];
+            //     for(int i = 1 ; i < 9 ; i++)
+            //     {
+            //         strcat(str1," ");
+            //         strcat(str1, local[x][i]);
+                   
+            //     }
+            //     strcat(str1,"\0");
+            //     strcpy(results[index1],str1);  
+            //     index1++; 
+            // }
+            // else if(strcmp(local[x][3],"48")==0)
+            // {
+            //     char *str1 = local[x][0];
+            //     for(int i = 1 ; i < 9 ; i++)
+            //     {
+            //         strcat(str1," ");
+            //         strcat(str1, local[x][i]);
+                   
+            //     }
+            //     strcat(str1,"\0");
+            //   strcpy(results[index1],str1);  
+            //   index1++; 
+            // }
+    for(int i = 0;i<index1; i++)
+            printf("%s\n",results[i]);
     }
     MPI_Gather(&results[0], index1, MPI_CHAR, &globalresults,index1,MPI_CHAR,0,MPI_COMM_WORLD);
     if(rank == 0)
